6670d0f644a67e87bfa6d0517b10f65c24091969
RafaellSoares <rafaelsoares199911@gmail.com>
2022-08-05 16:29:34 -0300

    1 file modified, 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NewsItem from './components/NewsItem.jsx';
+import SideBar from './components/SideBar.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NewsItem />
+      <SideBar />
     </div>
   );
 }
